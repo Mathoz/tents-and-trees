ae12086ad0e0c637ed64af30ec14d5fea11be38c
Mathieu Lambertin <mathieu.lambertin03@gmail.com>
2021-02-16 18:21:15 +0100

    finish the bttns need to start the animations

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 let bttns = document.querySelectorAll(".cell");
-let trueTents = document.querySelectorAll(".trueTent");
+// let trueTents = document.querySelectorAll(".trueTent.tent");
 
 const click = (event) => {
     if (event.target.classList.contains("tree")){
@@ -17,14 +17,62 @@ const click = (event) => {
     else {
         event.target.classList.add("grass")
     }
-    console.log(trueTents.forEach(tent => tent.classList.contains("tent")));
+    
+    // if(endGame() === true){}
 };
 
 bttns.forEach(bttn => bttn.onclick = click)
 
 function endGame(){
-    if(trueTents.forEach(tent => tent.classList.contains("tent"))){
+    if(document.querySelectorAll(".trueTent.tent").length === 7){
         return true
     }
     else{return false}
 }
+
+// INFOS
+// const info = (event) => {// mettre info visible}
+// document.getElementById("info").onclick = info()
+
+// MUSIC
+let theMusic = document.getElementById("theMusic")
+let bttnMusic = document.getElementById("musicOn")
+
+const music = (event) => {
+    if(bttnMusic.classList.contains("on")){
+        bttnMusic.classList.remove("on")
+        bttnMusic.classList.add("off")
+        bttnMusic.innerHTML = `<i class="fas fa-volume-mute"></i>`
+    }
+    else {
+        bttnMusic.classList.remove("off")
+        bttnMusic.classList.add("on")
+        bttnMusic.innerHTML = `<i class="fas fa-volume-up">`
+    }
+
+    if (bttnMusic.classList.contains("off")){
+        theMusic.pause()
+    }
+
+    if(bttnMusic.classList.contains("on")){
+        theMusic.play()
+    }
+}
+
+bttnMusic.onclick = music
+
+//REDO
+let redoBttn = document.getElementById("redo")
+
+const reDo = (event) => {
+    bttns.forEach(bttn => {
+        bttn.classList.remove("grass")
+        if (bttn.classList.contains("tent")){
+            bttn.classList.remove("tent")
+            bttn.innerHTML = ""
+        }
+    })
+}
+
+redoBttn.onclick = reDo
+
